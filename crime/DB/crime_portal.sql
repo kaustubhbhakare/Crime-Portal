de35e2b4711414fe6918f25e02a1e37b73f5be35
kaustubhbhakare <147875349+kaustubhbhakare@users.noreply.github.com>
2023-10-14 00:25:31 +0530

    Add files via upload

---
+++ crime/DB/crime_portal.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 17, 2018 at 04:19 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crime_portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint`
+--
+
+CREATE TABLE `complaint` (
+  `c_id` int(11) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `type_crime` varchar(50) NOT NULL,
+  `d_o_c` date NOT NULL,
+  `description` varchar(7000) NOT NULL,
+  `inc_status` varchar(50) DEFAULT 'Unassigned',
+  `pol_status` varchar(50) DEFAULT 'null',
+  `p_id` varchar(50) DEFAULT 'Null'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+INSERT INTO `complaint` (`c_id`, `a_no`, `location`, `type_crime`, `d_o_c`, `description`, `inc_status`, `pol_status`, `p_id`) VALUES
+(1, 123214521452, 'akola', 'Robbery', '2020-12-06', 'My Home has been Robbed.', 'Assigned', 'ChargeSheet Filed', 't101');
+
+
+
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `u_name` varchar(50) NOT NULL,
+  `u_id` varchar(50) NOT NULL,
+  `u_pass` varchar(50) NOT NULL,
+  `u_addr` varchar(100) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `gen` varchar(15) NOT NULL,
+  `mob` bigint(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`u_name`, `u_id`, `u_pass`, `u_addr`, `a_no`, `gen`, `mob`) VALUES
+('kaustubh', 'kaustubhbhakare7767@gmail.com', 'kaustubh', 'akola', 896873300263, 'Male', 9096455067);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD PRIMARY KEY (`c_id`);
+
+
+
+
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`a_no`),
+  ADD UNIQUE KEY `u_id` (`u_id`),
+  ADD UNIQUE KEY `mob` (`mob`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ crime/home.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+	<title>Crime Portal</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="home.css">
+    
+</head>
+    
+<body>
+    <body style="background-size: cover;
+    background-image: url(crime3.jpeg);
+    background-position: center;">
+ <nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="home.php"><b>Crime Portal</b></a>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="home.php">Home</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="userlogin.php">User Login  <i class="fa fa-user"></i></a></li>
+      </ul>
+    </div>
+  </div>
+ </nav>
+
+
+
+
+  
+  </div>
+  <div class="container">
+ 	  <div class="row">
+ 		  <div class="col-lg-12">
+ 			  <div class="content">
+ 				  <div class="for-img">
+            <h1> Have Any Complain?<h1>
+ 				    <h3>Register Here &nbsp &nbsp<i class="fa fa-hand-o-down" aria-hidden="true"></i></ h3>
+ 				    <hr>
+            <a href="registration.php" class="btn btn-default btn-lg" role="button" aria-pressed="true">Sign Up!</a>
+          </div>
+ 			  </div>
+ 		  </div>
+ 	  </div>
+  </div>
+  <div style="position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   height: 30px;
+   background-color: rgba(0,0,0,0.8);
+   color: white;
+   text-align: center;">
+  <h4 style="color: white;">&copy <b>kaustubhbhakare</b></h4>
+ </div>
+
+ <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.js"></script>
+ <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    
+</body>
+</html>
